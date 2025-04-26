64ed14fdad32cd04e02bdd75009b7704590d0fd6
Ori Hendel <orisolhendel@gmail.com>
2025-04-26 18:01:57 +0300

    adding  react

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask!"
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
     app.run(debug=True)
